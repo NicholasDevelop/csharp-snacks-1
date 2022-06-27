f26f9fa0b034dc2b4112138a413db8d2fc12a7a7
NicholasDevelop <marcaccini.n@gmail.com>
2022-06-27 14:09:39 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*Snack3 - - Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+ */
